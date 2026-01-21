c3a9288907c6a1b0d23018193bbd40fc74509bb0
krunal201 <krunalatulbhai@gmail.com>
2026-01-21 15:35:57 +0530

    Add & Alter column

---
+++ Queries/addStatusColumn.php
@@ -0,0 +1,13 @@
+<?php 
+    include('../connect.php');
+
+    // $addColumn="alter table user add column status char(6)";
+    $addColumn="alter table user modify column status varchar(6)";
+    $res=mysqli_query($con,$addColumn);
+    if($res){
+        // echo "Column Added";
+        echo "Column Altered";
+    }else{
+        echo "Fail";
+    }
+?>
