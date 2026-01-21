b216c407f70ed84a0bc273f51f7ac78d67608b08
krunal201 <krunalatulbhai@gmail.com>
2026-01-21 16:55:17 +0530

    New File

---
+++ Day2/Queries/createTable.php
@@ -0,0 +1,22 @@
+<?php 
+    require('../connect.php');
+
+    $crateTableQry="CREATE TABLE employees(
+        eId int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname varchar(50) NOT NULL,
+        lastname varchar(50) NOT NULL,
+        email varchar(20),
+        office_code varchar(50),
+        office_city varchar(50),
+        salary int(7),
+        join_date date,
+        dId int(4)
+    )";
+
+    $res=mysqli_query($con,$crateTableQry);
+    if($res){
+        echo "Table Created";
+    }else{
+        echo "Failed to create table";
+    }
+?>
