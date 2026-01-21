a5b5c531c9a68f0800d52ac63075d8b94440a4fc
krunal201 <krunalatulbhai@gmail.com>
2026-01-21 18:35:35 +0530

    Small Changes

--- Day2/Queries/insertData.php
+++ Day2/Queries/insertData.php
@@ -12,7 +12,7 @@
     ('David', 'Martinez','david.martinez@email.com', 2, 'Los Angeles', 'Director', 85000, '2017-11-23', 5),
     ('Emma', 'Taylor','emma.taylor@email.com', 1, 'New York', 'Sales Rep', 22000,  '2020-09-10', 4),
     ('Frank', 'Anderson', 'frank.anderson@email.com', 1, 'New York', 'Manager', 60000, '2015-07-05', 2),
-    ('Grace', 'Thomas', 3, 'Chicago', 'Sales Rep', 31000, 'grace.thomas@email.com', '2021-04-19', 1),
+    ('Grace', 'Thomas','grace.thomas@email.com', 3, 'Chicago', 'Sales Rep', 31000,  '2021-04-19', 1),
     ('Hannah', 'Jackson','hannah.jackson@email.com', 2, 'Los Angeles', 'Sales Rep', 20000,  '2020-02-22', 3),
     ('Ivy', 'White', 'ivy.white@email.com',  1, 'New York', 'Sales Manager', 72000,'2021-05-09', 4),
     ('James', 'Harris','james.harris@email.com', 3, 'Chicago', 'Sales Rep', 28000,  '2021-08-17', 5),
